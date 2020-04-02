56984532692692a32975dac7f45550c3b913fedf
James Bates <j4bates@gmail.com>
2020-04-02 15:10:39 -0700

    firebase

--- dist/src.a2b27638.js
+++ dist/src.a2b27638.js
@@ -36263,7 +36263,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63238" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57954" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
